252ca28daf849dd49d887e914e56bfcd994c9f15
Logan <loganr365@yahoo.com>
2017-02-25 16:40:54 -0500

    Sun and Eath finished, Moon messed up.

--- Rotations Demo/AppClass.cpp
+++ Rotations Demo/AppClass.cpp
@@ -1,11 +1,11 @@
 #include "AppClass.h"
 void AppClass::InitWindow(String a_sWindowName)
 {
-	super::InitWindow("Sandbox"); // Window Name
+	super::InitWindow("W5R_Rotations_DEMO"); // Window Name
 
-	// Set the clear color based on Microsoft's CornflowerBlue (default in XNA)
-	//if this line is in Init Application it will depend on the .cfg file, if it
-	//is on the InitVariables it will always force it regardless of the .cfg
+											 // Set the clear color based on Microsoft's CornflowerBlue (default in XNA)
+											 //if this line is in Init Application it will depend on the .cfg file, if it
+											 //is on the InitVariables it will always force it regardless of the .cfg
 	m_v4ClearColor = vector4(0.4f, 0.6f, 0.9f, 0.0f);
 	m_pSystem->SetWindowResolution(RESOLUTIONS::C_1280x720_16x9_HD);
 	//m_pSystem->SetWindowFullscreen(); //Sets the window to be fullscreen
@@ -21,7 +21,7 @@ void AppClass::InitVariables(void)
 		vector3(0.0f, 2.5f, 15.0f),//Camera position
 		vector3(0.0f, 2.5f, 0.0f),//What Im looking at
 		REAXISY);//What is up
-	//Load a model onto the Mesh manager
+				 //Load a model onto the Mesh manager
 	m_pMeshMngr->LoadModel("Minecraft\\Steve.obj", "Steve");
 }
 
@@ -39,12 +39,15 @@ void AppClass::Update(void)
 
 	//Call the arcball method
 	ArcBall();
-	
+
 	//Set the model matrix for the first model to be the arcball
-	m_pMeshMngr->SetModelMatrix(ToMatrix4(m_qArcBall), 0);
-	
-	//m_m4Steve = glm::scale(m_m4Steve, vector3(0.0f, 0.0f, 0.0f));
-	m_m4Steve = glm::rotate(m_m4Steve, 1.0f, vector3(0.0f, 0.0f, 0.0f));
+	//m_pMeshMngr->SetModelMatrix(ToMatrix4(m_qArcBall), 0);
+
+	//m_m4Steve = glm::scale(m_m4Steve, vector3(1.01f, 1.01f, 1.01f));
+	//m_m4Steve = glm::rotate(m_m4Steve, 1.0f, vector3(0.0f, 1.0f, 0.0f));
+	m_m4Steve = glm::rotate(IDENTITY_M4, m_v3Orientation.x, vector3(1.0f, 0.0f, 0.0f));
+	m_m4Steve = glm::rotate(m_m4Steve, m_v3Orientation.y, vector3(0.0f, 1.0f, 0.0f));
+	m_m4Steve = glm::rotate(m_m4Steve, m_v3Orientation.z, vector3(0.0f, 0.0f, 1.0f));
 	m_pMeshMngr->SetModelMatrix(m_m4Steve, "Steve");
 
 	//Adds all loaded instance to the render list
@@ -56,11 +59,12 @@ void AppClass::Update(void)
 	//print info into the console
 	//printf("FPS: %d            \r", nFPS);//print the Frames per Second
 	//Print info on the screen
+	m_pMeshMngr->PrintLine("");
 	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
 
 	m_pMeshMngr->Print("Selection: ");
 	m_pMeshMngr->PrintLine(m_pMeshMngr->GetInstanceGroupName(m_selection.first, m_selection.second), REYELLOW);
-	
+
 	m_pMeshMngr->Print("FPS:");
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
 }
@@ -74,7 +78,6 @@ void AppClass::Display(void)
 	m_pMeshMngr->Render(); //renders the render list
 	m_pMeshMngr->ClearRenderList(); //Reset the Render list after render
 	m_pGLSystem->GLSwapBuffers(); //Swaps the OpenGL buffers
-
 }
 
 void AppClass::Release(void)
