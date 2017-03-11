92b23ae04dcc2daff6777ff9d9d1f4945c6d3fdd
MAIN\ltr6702 <loganr365@yahoo.com>
2017-03-11 16:04:57 -0500

    A06 Finished.

--- A06_LERP/AppClass.cpp
+++ A06_LERP/AppClass.cpp
@@ -14,6 +14,20 @@ void AppClass::InitVariables(void)
 	m_pMeshMngr->LoadModel("Sorted\\WallEye.bto", "WallEye");
 
 	fDuration = 1.0f;
+
+	points = {
+		vector3(-4.0f, -2.0f, 5.0f),
+		vector3(1.0f, -2.0f, 5.0f),
+		vector3(-3.0f, -1.0f, 3.0f),
+		vector3(2.0f, -1.0f, 3.0f),
+		vector3(-2.0f, 0.0f, 0.0f),
+		vector3(3.0f, 0.0f, 0.0f),
+		vector3(-1.0f, 1.0f, -3.0f),
+		vector3(4.0f, 1.0f, -3.0f),
+		vector3(0.0f, 2.0f, -5.0f),
+		vector3(5.0f, 2.0f, -5.0f),
+		vector3(1.0f, 3.0f, -5.0f)
+	};
 }
 
 void AppClass::Update(void)
@@ -45,11 +59,23 @@ void AppClass::Update(void)
 
 	//Indicate the FPS
 	int nFPS = m_pSystem->GetFPS();
+	int rounds = 0;
+	const float ROUND_TIME = 55.0f;
+	const float TRAVEL_TIME = 5.0f;
+	static bool completed = false;
 
 	//Print info on the screen
 	static DWORD startTimeSystem = GetTickCount();
+	static int currentPoint = 0;
 	DWORD timeApplication = GetTickCount() - startTimeSystem;
 	float timer = timeApplication / 1000.0f;
+
+	for (int i = 0; i < points.size(); i++) {
+		matrix4 m4Location = IDENTITY_M4;
+		m4Location = glm::scale(0.5f, 0.5f, 0.5f);
+		m4Location = glm::translate(points[i]);
+		m_pMeshMngr->AddSphereToRenderList(m4Location, vector3(0.5f, 0.0f, 0.0f), 1);
+	}
 
 	m_pMeshMngr->PrintLine("");
 	m_pMeshMngr->PrintLine(m_pSystem->GetAppName(), REYELLOW);
@@ -58,9 +84,21 @@ void AppClass::Update(void)
 	m_pMeshMngr->Print(std::to_string(nFPS), RERED);
 
 	matrix4 m4WallEye;
-	float timerMapped = MapValue(timer, 0.0f, 5.0f, 0.0f, 1.0f);
-	if (timerMapped > 1.0f) { timerMapped = 1.0f; }
-	vector3 v3Lerp = glm::lerp(vector3(0, 0, 0), vector3(1, 0, 0), timerMapped);
+	vector3 v3Lerp;
+	
+	if (!completed) 
+	{
+		float timerMapped = MapValue(timer, 0.0f + (currentPoint * TRAVEL_TIME + (ROUND_TIME * rounds)), 5.0f + (currentPoint * TRAVEL_TIME + (ROUND_TIME * rounds)), 0.0f, 1.0f);
+		if (timerMapped > 1.0f) {
+			timerMapped = 0.0f;
+			currentPoint++;
+		}
+
+		if (currentPoint >= points.size()) { currentPoint = 0; rounds++; completed = true; }
+		else if (currentPoint < points.size() - 1) { v3Lerp = glm::lerp(points[currentPoint], points[currentPoint + 1], timerMapped); }
+		else if (currentPoint == points.size() - 1) { v3Lerp = glm::lerp(points[currentPoint], points[0], timerMapped); }
+	}
+
 	//m4WallEye = glm::translate(vector3(1.0f, 0.0f, 0.0f));
 	m4WallEye = glm::translate(v3Lerp);
 	m_pMeshMngr->SetModelMatrix(m4WallEye, "WallEye");
